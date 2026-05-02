06f67eae118cbfc0ad3712037a6b3d6b9b1625f4
Aaditya Raj <ar912739@gmail.com>
2026-05-02 11:00:00 +0530

    ADD Unique Number of Occurrences

--- Unique Number of Occurrences/Unique Number of Occurrences.java
+++ Unique Number of Occurrences/Unique Number of Occurrences.java
@@ -1,22 +1,26 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         HashSet<Integer> set = new HashSet<>();
         int i = 0;
         int n = arr.length;
-        while(i < n){
-            if(!map.containsKey(arr[i])){
-                map.put(arr[i],1);
-            }else{
-                map.put(arr[i],map.get(arr[i])+1);
-                if(!set.contains(map.get(arr[i]))){
-                    set.add(map.get(arr[i]));
-                }else{
-                    return true;
-                }
+        while (i < n) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], 1);
+            } else {
+                map.put(arr[i], map.get(arr[i]) + 1);
             }
             i++;
         }
-        return false;
+        int j = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int freq = entry.getValue();
+            if(set.contains(freq)){
+                return false;
+            }else{
+                set.add(freq);
+            }
+        }
+        return true;
     }
 }
